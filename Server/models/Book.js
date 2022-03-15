2ae4d7b5de23ba190980da31bf1613bd4a5cd082
Dawit-Zeleke-Admassu <dadmassu@miu.edu>
2022-03-14 22:43:06 -0500

    get single book and get all book functionality done

--- Server/models/Book.js
+++ Server/models/Book.js
@@ -1,24 +1,11 @@
-const mongoose=require('mongoose')
-const Book=mongoose.Schema({
-book_id:{type:String,required:true,index:true },
-Title:{type:String,required:true,index:true },
-TotalPage:{type:Number,required:true },
-ISBN:{type:String,required:true,index:true },
-publish_date:{type:Date,required:true},
-Edition:{type:String,required:true},
-CalculatedRating:{type:Number},
-comment:[Comments],
-Authors:[Author],
-rating:[rate]
-}
-);
+const  mongoose = require('mongoose')
+
 const Comments=mongoose.Schema({
     user_id:{type:String},
     Title:{type:String},
     Body:{type:String,required:true},
     Data:{type:Date}
 });
-
 const Author=mongoose.Schema({
     First_name:{type:String,index:true},
     Middle_name:{type:String},
@@ -26,10 +13,20 @@ const Author=mongoose.Schema({
     Birth_day:{type:Date},
     Country:{type:String}
 });
-const rate=({
-    user_id:{type:String},
-    rating:{type:Number}
-})
+
+const Book=mongoose.Schema({
+book_id:{type:String,required:true,index:true },
+Title:{type:String,required:true,index:true },
+TotalPage:{type:Number,required:true },
+ISBN:{type:String,required:true,index:true },
+publish_date:{type:Date,required:true},
+Edition:{type:String,required:true},
+comment:[Comments],
+Authors:[Author]
+}
+);
+
+
 
 
 module.exports=mongoose.model('Books',Book);
---
+++ Server/routes/book.js
@@ -0,0 +1,11 @@
+const express =require('express');
+const bookController = require('../controllers/book')
+const varifyTokenMiddleware =require('../middlewares/varifyToken')
+const router = express.Router();
+
+
+
+router.get('/' ,bookController.getAllbooks)
+router.get('/:id' ,bookController.getSingleBook)
+
+module.exports =router;
